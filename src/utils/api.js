1a1f4d906d393fba33b55523f6d7113e04e0aae3
Sheng Wu <shwu1@4180L-144244-M.local>
2018-09-19 14:09:58 +1000

    finished plots

--- src/utils/api.js
+++ src/utils/api.js
@@ -33,6 +33,62 @@ function getPlots() {
     });
 }
 
+function formatPlots() {
+  return Promise.resolve(
+    getPlots()
+  ).then((plots) => {
+    let formattedPlots = [];
+    let i;
+
+    for (i = 0; i < plots.length; i++) {
+      const pens = plots[i].pens;
+      let formattedDataset = [];
+      let j;
+      for (j = 0; j < pens.length; j++) {
+        const points = pens[j].points;
+        formattedDataset.push(
+          {
+            label: pens[j].name,
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: pens[j].color,
+            borderColor: pens[j].color,
+            borderCapStyle: 'butt',
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: pens[j].color,
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: pens[j].color,
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: points
+          }
+        )
+      }
+
+      formattedPlots.push({
+        formattedData: {
+          datasets: formattedDataset
+        },
+        options: {
+          legend: {
+            display: plots[i].legendShown
+          },
+          title: {
+            display: true,
+            text: plots[i].name
+          }
+        }
+      })
+    }
+    return formattedPlots
+  })
+}
+
 export function getData() {
   return Promise.resolve(
     axios.post(`http://localhost:8080/trust-model/setup`)
@@ -40,7 +96,8 @@ export function getData() {
       return Promise.all([
         getReports(),
         getView(),
-        getPlots()
+        // getPlots(),
+        formatPlots()
       ]).then(([reports, view, plots]) => ({reports, view, plots}));
     })
     .catch(error => {
