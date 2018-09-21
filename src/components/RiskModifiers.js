dc80c11848a485df0f619b273e62579013b769c2
Sheng Wu <shwu1@4180L-144244-M.local>
2018-09-21 15:55:04 +1000

    needs to finish map function in Risk Modifiers

--- src/components/RiskModifiers.js
+++ src/components/RiskModifiers.js
@@ -3,90 +3,102 @@ import {connect} from 'react-redux';
 import '../styles/riskModifiers.css';
 import {formatNumber} from "../utils/helpers";
 import Loading from "./Loading";
+import {handleUpdateReport} from "../actions/reports";
 
 class RiskModifiers extends Component {
   render() {
     if (this.props.reports.injurySeverity_Label !== undefined) {
       return (
         <div className='risk-modifiers-container text-center'>
-          <h4 id='risk-modifiers-container-header'>Recovery Risk Modifiers</h4>
+          <h4 id='risk-modifiers-container-header'>Modifiers</h4>
           <div id='risk-modifiers-container-body'>
             <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
+              <span><button type="button" className="btn btn-outline-primary"
+                        onClick={() => this.props.dispatch(handleUpdateReport({
+                          name:'injurySeverity_Label',
+                          value:this.props.reports.injurySeverity_Label - 1},
+                          'ask clients [ set InjurySeverity InjurySeverity - 1 ]'
+                          ))}>
+                - 1</button></span>
               <span>{formatNumber(this.props.reports.injurySeverity_Label, 3)}</span>
               <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
             </p>
             <label>Injury Severity</label>
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.atFaultStatus_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>At Fault Status</label>
+            Object
+            <p><span>
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.healthStatus_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Health Status</label>
+            </span></p>
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.previousInjury_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Previous Injury</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.atFaultStatus_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>At Fault Status</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.employmentStatus_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Employment</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.healthStatus_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Health Status</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.vulnerableStatus_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Vulnerable Status</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.previousInjury_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Previous Injury</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.gender_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Gender</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.employmentStatus_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Employment</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.age_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Age</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.vulnerableStatus_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Vulnerable Status</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.claimDuration_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Claim Duration</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.gender_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Gender</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.injuryClassification_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Injury Classification</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.age_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Age</label>*/}
 
-            <p>
-              <span><button type="button" className="btn btn-outline-primary">- 1</button></span>
-              <span>{formatNumber(this.props.reports.education_Label, 3)}</span>
-              <span><button type="button" className="btn btn-outline-primary">+ 1</button></span>
-            </p>
-            <label>Education</label>
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.claimDuration_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Claim Duration</label>*/}
+
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.injuryClassification_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Injury Classification</label>*/}
+
+            {/*<p>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">- 1</button></span>*/}
+            {/*<span>{formatNumber(this.props.reports.education_Label, 3)}</span>*/}
+            {/*<span><button type="button" className="btn btn-outline-primary">+ 1</button></span>*/}
+            {/*</p>*/}
+            {/*<label>Education</label>*/}
           </div>
         </div>
       )
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,21 +1,8 @@
-export function isObject(item) {
-  return Object.prototype.toString.call(item) === '[object Object]'
-}
-
-export function getPercentage(count, total) {
-  return total === 0 ? 0 : parseInt(count / total * 100, 10)
-}
-
 export function formatNumber(number, digit) {
-  // console.log('formatNumber', number);
   if(number) {
     const formattedNumber = number.toFixed(digit);
     return formattedNumber;
   } else {
     return number
   }
-}
-
-export function convertCordinate(cordinate, adjustment) {
-  return Number(cordinate) + adjustment
 }
